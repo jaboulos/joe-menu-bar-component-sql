4b71d49df677e135eadb9dbb1f68ee0347f3452c
Joe Boulos <jab12288@gmail.com>
2019-02-11 15:34:13 -0800

    users schema

---
+++ migrations/20190211150332_Users.js
@@ -0,0 +1,63 @@
+/*
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', (table) => {
+    // user_id increments, primary
+    table.increments('user_id').primary();
+    // display_name string
+    table.string('display_name');
+    // logo string
+    table.string('logo');
+    // profile_image_url string
+    table.string('profile_image_url');
+    // category string
+    table.string('category');
+    // followers integer
+    table.integer('followers');
+    // Following integer
+    table.integer('Following');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users');
+};
+
+// run the migration in terminal
+// knex migrate: latest
+*/
+
+
+exports.up = function(knex, Promise) {
+  knex.schema.dropTableIfExists('users')
+  .then(() => {
+    return knex.schema.createTable('users', (table) => {
+      // user_id increments, primary
+      table.increments('user_id').primary();
+      // display_name string
+      table.string('display_name');
+      // logo string
+      table.string('logo');
+      // profile_image_url string
+      table.string('profile_image_url');
+      // category string
+      table.string('category');
+      // followers integer
+      table.integer('followers');
+      // Following integer
+      table.integer('Following');
+    })
+  })
+  .then(() => {
+    // table created
+  })
+  .catch((error) => {
+    console.log('error: ', error);
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users');
+};
+
+// run the migration in terminal
+// knex migrate: latest
