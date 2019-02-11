317cb3226ecf36f6bf9c7daaa3b68aefe15dfcd1
Joe Boulos <jab12288@gmail.com>
2019-02-11 15:49:00 -0800

    working on knex seed script

--- seeds/01_user.js
+++ seeds/01_user.js
@@ -1,3 +1,4 @@
+const fakeData = require('../fakeData')
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
@@ -5,10 +6,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       // create an object that has all of the properties of the table you created
-      return knex('users').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
+      return knex('users').insert(fakeData);
     });
 };
+
+// run the seed
+// knex seed:run
