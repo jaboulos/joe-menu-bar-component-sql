01579d19d52c97c6611d0ecf6b3bd793342cc3cf
Joe Boulos <jab12288@gmail.com>
2019-02-11 19:46:07 -0800

    created schema for knex and postgres

--- migrations/20190211150332_Users.js
+++ migrations/20190211150332_Users.js
@@ -1,6 +1,5 @@
-/*
-exports.up = function(knex, Promise) {
-  return knex.schema.createTable('users', (table) => {
+exports.up = async function(knex, Promise) {
+  await knex.schema.createTable('users', (table) => {
     // user_id increments, primary
     table.increments('user_id').primary();
     // display_name string
@@ -18,48 +17,6 @@ exports.up = function(knex, Promise) {
   })
 };
 
-exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('users');
+exports.down = async function(knex, Promise) {
+  await knex.schema.dropTable('users');
 };
-
-// run the migration in terminal
-// knex migrate: latest
-*/
-
-exports.up = function(knex, Promise) {
-  // knex.schema.dropTableIfExists('users')
-  // .then(() => {
-    return knex.schema.createTable('users', (table) => {
-      // user_id increments, primary
-      table.increments('user_id').primary();
-      // display_name string
-      table.string('display_name');
-      // logo string
-      table.string('logo');
-      // profile_image_url string
-      table.string('profile_image_url');
-      // category string
-      table.string('category');
-      // followers integer
-      table.integer('followers');
-      // Following integer
-      table.integer('Following');
-    })
-  //})
-  .then(() => {
-    // table created
-  })
-  .catch((error) => {
-    console.log('error: ', error);
-  })
-};
-
-exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('users');
-};
-
-// run the migration in terminal
-// knex migrate:latest
-
-// now that this works, create the seed with the command
-// knex seed:make 01_user
