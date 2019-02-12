e076426a960b5b669c82172a0685d49e63855237
Joe Boulos <jab12288@gmail.com>
2019-02-11 19:45:13 -0800

    code cleanup

--- fakeData.js
+++ fakeData.js
@@ -19,28 +19,3 @@ module.exports = [
 ];
 
 
-/*
-module.exports = async function seedUser(outerLoop, innerLoop) {
-  for(let i = 0; i < outerLoop; i++) {
-    // need to insert an array of objects into the db
-    let arr = [];
-    for(let j = 0; j < innerLoop; j++) {
-      let userObject = {
-        // user_id: faker.random.number(),
-        display_name: faker.internet.userName(),
-        logo: faker.image.imageUrl(),
-        profile_image_url: faker.internet.url(),
-        category: faker.company.bsAdjective(),
-        followers: faker.random.number(),
-        Following: faker.random.number(),
-      }
-      arr.push(userObject);
-    }
-    await UserDb.bulkCreate(arr);
-  }
-}
-
-// seedUser(1000, 10000);
-seedUser(1, 10);
-*/
-
