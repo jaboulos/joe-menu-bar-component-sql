e0cee966ae2b8e9c277edc60c3bcb1f8332322da
Joe Boulos <jab12288@gmail.com>
2019-02-17 12:55:22 -0800

    outputs file, almost there, not correct csv format

--- csv_generator.js
+++ csv_generator.js
@@ -1,16 +1,16 @@
-// const csvWriter = require('csv-write-stream');
-// const fs = require('file-system');
-
+/*
+// WORKING VERSION
+// node --max-old-space-size=4096 somescript.js
 const faker = require('faker');
 faker.locale = 'en_US';
 
-// stack overflow recommendation, node property
 require('events').EventEmitter.prototype._maxListeners = 1000;
 
-function fakeData() {
+
+const final = function fakeData() {
   // usersArr is the array that i want to turn into a csv
   const data = [];
-  for(let i = 0; i < 10; i++) {
+  for(let i = 0; i < 100; i++) {
     const makeUser = () => ({
       user_id: i+1,
       display_name: faker.internet.userName(),
@@ -22,11 +22,7 @@ function fakeData() {
     })
     const obj = makeUser()
     data.push(obj)
-
-    // const csvData = objectToCsv(data)
   }
-  // return data;
-
   // this is the function to turn the object into a csv
   const objectToCsv = function(data) {
     // final output should be a bunch of csv rows, newline separated rows
@@ -36,33 +32,91 @@ function fakeData() {
       // get the keys from the first object in the array
     const headers = Object.keys(data[0]);
     csvRows.push(headers.join(','));
-    // console.log(headers);
     // loop over the rows
     for(const row of data) {
       // need to map over the headers and make sure theyre in the same order every time
       // the value of const values should be the 7 values for each row
       const values = headers.map(header => {
-        // need to escape quotes
-        // need to coerce value into a string in case you have numbers, cant use .replace on numbers
+        // need to escape quotes, need to coerce value into a string in case you have numbers, cant use .replace on numbers
         const escaped = (''+row[header]).replace(/"/g, '\\"')
         // row[header] is the first header
         // need to wrap each escaped value in a quote
         return `"${escaped}"`
-        //return `"${row[header]}"`
       })
-      // console.log(values.join(','));
       // now push these values into the csvRows array, join by comma
       csvRows.push(values.join(','));
     }
     // join everything by newline, cant have an array in a csv
     return csvRows.join('\n');
   }
-  const csvData = objectToCsv(data);
-  console.log(csvData);
+  //console.log(objectToCsv(data));
+
 };
 
+final();
+
+// console.log(final());
+*/
 
 
+///////testing///////////
 
-fakeData();
+// WORKING VERSION
+// node --max-old-space-size=4096 somescript.js
+const fs = require('fs');
+const faker = require('faker');
+faker.locale = 'en_US';
+
+require('events').EventEmitter.prototype._maxListeners = 1000;
+
+
+const final = function fakeData() {
+  // usersArr is the array that i want to turn into a csv
+  const data = [];
+  for(let i = 0; i < 100; i++) {
+    const makeUser = () => ({
+      user_id: i+1,
+      display_name: faker.internet.userName(),
+      logo: faker.image.imageUrl(),
+      profile_image_url: faker.internet.url(),
+      category: faker.company.bsAdjective(),
+      followers: faker.random.number(),
+      following: faker.random.number(),
+    })
+    const obj = makeUser()
+    data.push(obj)
+  }
+  // this is the function to turn the object into a csv
+  const objectToCsv = function(data) {
+    // final output should be a bunch of csv rows, newline separated rows
+    const csvRows = [];
+    // get the headers for the csv
+    // headers of the json object will be the keys from one of them
+      // get the keys from the first object in the array
+    const headers = Object.keys(data[0]);
+    csvRows.push(headers.join(','));
+    // loop over the rows
+    for(const row of data) {
+      // need to map over the headers and make sure theyre in the same order every time
+      // the value of const values should be the 7 values for each row
+      const values = headers.map(header => {
+        // need to escape quotes, need to coerce value into a string in case you have numbers, cant use .replace on numbers
+        const escaped = (''+row[header]).replace(/"/g, '\\"')
+        // row[header] is the first header
+        // need to wrap each escaped value in a quote
+        return `"${escaped}"`
+      })
+      // now push these values into the csvRows array, join by comma
+      csvRows.push(values.join(','));
+    }
+    // join everything by newline, cant have an array in a csv
+    return csvRows.join('\n');
+  }
+  //console.log(objectToCsv(data));
+  fs.writeFileSync('data1', objectToCsv(data), 'utf8');
+};
+
+final();
+
+
 
